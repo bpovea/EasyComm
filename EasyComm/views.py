c2d855cccdee988853a91f843486069cf0fcf55c
mdleiton <mau_leiton96@hotmail.com>
2018-08-18 02:34:16 -0500

    [UPD] enlaces del menu actualizados, estilos, opciones promociones, socios, productos

--- EasyComm/views.py
+++ EasyComm/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,render_to_response,get_object_or_404,redirect
 from django.http import JsonResponse
 import json
+from oscar.core.loading import get_class, get_model
+
+
+Partner = get_model('partner', 'Partner')
 
 def home(request):
-	return render(request,'../templates/EasyComm/index.html')
+	partners = Partner.objects.all()[:3]
+	return render(request,'../templates/EasyComm/index.html',{'partners':partners})
 
 
 def about_us(request):
@@ -14,6 +19,11 @@ def contact_us(request):
 
 def faqs(request):
 	return render(request,'../templates/EasyComm/faqs.html')
+
+
+def partners(request):
+	partners = Partner.objects.all()
+	return render(request,'../templates/EasyComm/partners.html',{'partners':partners})
 
 
 def faqs_load(request):
